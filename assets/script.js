e8299a37dbbc947c98c0b0781be224c21b4936ec
Gustavo Receputi <receputi27dev@gmail.com>
2026-06-24 13:44:30 -0300

    Pasta de Assets
    Adicionado os arquivos de javascript e css, junto com os ID's

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+const apiKeyInput = document.getElementById('apiKey');
+const gameSelect = document.getElementById('gameSelect');
+const questionInput = document.getElementById('questionInput');
+const askButton = document.getElementById('askButton');
+const aiResponde = document.getElementById('aiResponse');
+const form = document.getElementById('form');
